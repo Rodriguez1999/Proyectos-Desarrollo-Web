a088e2cad07d16ac49cac288faa3bcef3ee7bc45
Nelson Rivera <isaacriverahn7@gmail.com>
2021-11-17 20:25:08 -0600

    Commit inicial de vistas de la app administrativa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginLandingComponent } from './components/login-landing/login-landing.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DashboardSidebarComponent } from './components/dashboard-sidebar/dashboard-sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { CompaniesComponent } from './components/companies/companies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginLandingComponent,
+    SidebarComponent,
+    DashboardSidebarComponent,
+    CompaniesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
